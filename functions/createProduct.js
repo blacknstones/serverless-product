facff9e75701b66d71c18b7ffa10ddf8711dc0e0
Yanwei Zhu <47001967+blacknstones@users.noreply.github.com>
2021-07-15 11:34:12 +0200

    initial build
    Co-authored-by: Yones-je <Yones-je@users.noreply.github.com>
    Co-authored-by: Anton Bertilsson <Imaginaryverse@users.noreply.github.com>

---
+++ functions/createProduct.js
@@ -0,0 +1,32 @@
+import faunadb from 'faunadb';
+
+const q = faunadb.query;
+const client = new faunadb.Client({
+  secret: process.env.DB_KEY,
+});
+
+exports.handler = async (event, context) => {
+  const data = await JSON.parse(event.body);
+
+  console.log(`Creating product...`, data);
+
+  const product = {
+    data,
+  };
+
+  return client
+    .query(q.Create(q.Ref('classes/products'), product))
+    .then(res => {
+      console.log(`Success!`, res);
+      return {
+          statusCode: 201,
+          body: JSON.stringify(res)
+      }
+    }).catch(err => {
+        console.log(`Error!`, err);
+        return {
+            statusCode: 400,
+            body: JSON.stringify(err)
+        }
+    });
+};
--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const test = {
+    name: 'Computer',
+    description: 'It computes things',
+    price: 2999
+  };
+
+const App = () => {
+  const addProduct = data => {
+    axios.post('/.netlify/functions/createProduct', data)
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => console.log(err))
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Add products to db</h1>
+      <button onClick={() => addProduct(test)}>Add product</button>
     </div>
   );
 }
