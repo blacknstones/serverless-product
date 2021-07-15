2fba9deecc9e67d2aa0ad7cc8d9a97b1b3f4e1d3
Yanwei Zhu <47001967+blacknstones@users.noreply.github.com>
2021-07-15 14:57:51 +0200

    get by id

--- functions/getProductById.js
+++ functions/getProductById.js
@@ -4,3 +4,22 @@ const q = faunadb.query;
 const client = new faunadb.Client({
   secret: process.env.DB_KEY,
 });
+
+exports.handler = (event, context) => {
+  const id = event.path.match(/([^\/]*)\/*$/)[0];
+  console.log(`Function 'product-read' invoked. Read id: ${id}`)
+  return client.query(q.Get(q.Ref(`classes/products/${id}`)))
+  .then((response) => {
+    console.log("success", response)
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response)
+    }
+  }).catch((error) => {
+    console.log("error", error)
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error)
+    }
+  })
+}
--- src/App.js
+++ src/App.js
@@ -4,48 +4,54 @@ import './App.css';
 import { v4 as uuidv4 } from 'uuid';
 
 const test = {
-    id: uuidv4(),
-    name: 'Computer',
-    description: 'It computes things',
-    price: 2999
-  };
+  id: uuidv4(),
+  name: 'Computer',
+  description: 'It computes things',
+  price: 2999,
+};
 
 const App = () => {
   const [products, setProducts] = useState([]);
+  const [product, setProduct] = useState({});
 
   const addProduct = data => {
-    axios.post('/.netlify/functions/createProduct', data)
-    .then(res => {
-      console.log(res)
-    })
-    .catch(err => console.log(err))
-  }
-  
+    axios
+      .post('/.netlify/functions/createProduct', data)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => console.log(err));
+  };
+
   const getProductList = async () => {
     const productList = await axios.get('/.netlify/functions/getAllProducts');
     console.log(productList);
-    setProducts(productList);
-  }
- 
+    setProducts(productList.data);
+  };
+
+  const getProductById = async id => {
+    const productDetails = await axios.get('/.netlify/functions/getAllProducts', id);
+    setProduct(productDetails);
+    console.log(`The product you selected is ${productDetails}`);
+
+  };
 
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Add products to db</h1>
       <button onClick={() => addProduct(test)}>Add product</button>
       <button onClick={() => getProductList()}>Get all products</button>
-      {
-        products &&
-        products.map(el => 
-        (<div key={Date.now()}>
-          <p>{el.name}</p>
-          <p>{el.description}</p>
-          <p>{el.price}</p>
-
-        </div>)
-        )
-      }
+      {products &&
+        products.map(el => (
+          <div key={el.data.id} onClick={() => getProductById(el.data.id)}>
+            <p>{el.data.id}</p>
+            <p>{el.data.name}</p>
+            <p>{el.data.description}</p>
+            <p>{el.data.price}</p>
+          </div>
+        ))}
     </div>
   );
-}
+};
 
 export default App;
